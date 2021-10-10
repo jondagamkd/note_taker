b21ff407ee5c6c3a8b2791dc6bf96f2d9111ee75
jondagamkd <johntaylorwoodland@gmail.com>
2021-10-10 02:46:17 -0700

    added delete function and readme

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
-# note_taker
+[![MIT License](https://img.shields.io/apm/l/atomic-design-ui.svg?)](https://github.com/tterb/atomic-design-ui/blob/master/LICENSEs)
+<br />
+<p align="center">
+  <h1 align="center">Note Taker</h1>
+</p>
+
+## Description 
+
+This handy little app can be used to take notes and it will save them on the server for later viewing.
+
+
+## Table of Contents
+
+* [Usage](#usage)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [License](#license)
+* [Questions](#questions)
+* [Links](#links)
+
+
+## Usage 
+
+1. Visit the site [HERE](https://desolate-taiga-67985.herokuapp.com/).
+2. Click the GET STARTED button.
+3. Enter your note title and description on the NOTE TITLE and NOTE TEXT areas.
+4. A little save icon will appear top right, click that and it is saved!
+* You can also delete notes by clicking the little trash icon next to the note.
+<br><br>
+
+[![screenshot](./public/assets/img/noteTaker.jpg)](https://desolate-taiga-67985.herokuapp.com/)
+
+
+## Contributing
+
+No contributions at this time please, but your are free to use this code in your own project given the license listed below.
+
+
+## Tests
+
+I welcome any tests or suggestions to better this project in the future.
+
+
+## License
+
+This project is covered under the MIT license.
+
+[![MIT License](https://img.shields.io/apm/l/atomic-design-ui.svg?)](https://github.com/tterb/atomic-design-ui/blob/master/LICENSEs)
+
+
+## Questions
+
+I can be contacted via email or on github at:
+
+* E-mail: johntaylorwoodland@gmail.com
+* Github: https://github.com/jondagamkd/
+
+## Links
+
+The site can be reached [HERE](https://desolate-taiga-67985.herokuapp.com/)
+
+
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-const notes = require("./db/db");
+//const notes = require("./db/db");
 const path = require('path');
 
 app.use(express.urlencoded({ extended: true }));
@@ -33,18 +33,56 @@ app.get('/notes', (req, res) => {
 
 //Get API Routes
 app.get("/api/notes", (req, res) => {
+    var notes = JSON.parse(fs.readFileSync(path.join(__dirname, './db/db.json'), 'utf8'));
     let results = notes;
     res.json(results);
   });
   
 // Post API Route
-app.post("/api/notes", (req, res) => {
+app.post("/api/notes", (req, res) => {;
+    var notes = JSON.parse(fs.readFileSync(path.join(__dirname, './db/db.json'), 'utf8'));
     // set id based on what the next index of the array will be
     req.body.id = notes.length.toString();
+
+    // Check to make sure the ID is unique
+    for (let index = 0; index < notes.length; index++) {
+        if (notes[index]['id'] && notes[index]['id'] === req.body.id) {
+            idInt = Number(req.body.id);
+            idInt += 1
+            req.body.id = idInt.toString();
+            index = 0
+        }
+        
+    }
   
     const animal = createNewPost(req.body, notes);
     res.json(animal);
   });
+
+function findById(id, array) {
+    const result = array.filter(notez => notez.id === id)[0];
+    return result;
+}
+
+app.delete("/api/notes/:id", (req, res) => {
+    var notes = JSON.parse(fs.readFileSync(path.join(__dirname, './db/db.json'), 'utf8'));
+    const result = findById(req.params.id, notes);
+    if (result) {
+        for (let index = 0; index < notes.length; index++) {
+            if (notes[index] === result) {
+                notes.splice(index, 1);
+                fs.writeFileSync(
+                    path.join(__dirname, './db/db.json'),
+                    JSON.stringify(notes, null, 2)
+                  );
+                break
+            }       
+        }
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+});
   
 
 // Cover all HTML Route
